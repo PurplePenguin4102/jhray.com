42386367c9dc181ed48cd20d1a9857e1c8262cc3
Joey <joseph.h.ray@gmail.com>
2017-11-09 20:35:09 +1000

    MEGA COMMIT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jhray.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
