8fc6cb818c634f95dd94b1a1d8d29b58593b56a5
Joey <joseph.h.ray@gmail.com>
2017-11-14 00:26:43 +1000

    expert advice

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -6,9 +6,9 @@ dotnet publish jhray.com.csproj
 cd /home/travis/build/PurplePenguin4102/jhray.com/bin/Debug/netcoreapp2.0/publish/
 
 git init
-git remote add deploy "deploy:7qt59947@jhray.com:/var/aspnetcore"
+git remote add deploy "deploy@jhray.com:/var/aspnetcore"
 git config user.name "Travis CI"
-git confi user.email "joseph.h.ray@gmail.com"
+git config user.email "joseph.h.ray@gmail.com"
 git add .
 git commit -m "Deploy"
 git push --force deploy master
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir .ssh
-chmod 600 $Deploy_Key
-echo $Deploy_Key > ~/.ssh/id_rsa
+mkdir -p ~/.ssh 
+chmod 700 ~/.ssh
+echo "$Deploy_Key" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
