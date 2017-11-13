df438a0239047dd332e9a0ea16112ea8db66c6e4
Joey <joseph.h.ray@gmail.com>
2017-11-13 22:55:32 +1000

    modifying stuff

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!bin/bash
-if [ $TRAVIS_BRANCH == 'master'] ; then
-	echo "Running deploy script on master branch"
-	dotnet publish jhray.com.csproj
-	cd /home/travis/build/PurplePenguin4102/jhray.com/bin/Debug/netcoreapp2.0/publish/
-	git init
-	git remote add deploy "deploy@jhray.com:/var/aspnetcore"
-	git config user.name "Travis CI"
-	git confid user.email "joseph.h.ray@gmail.com"
 
-	git add .
-	git commit -m "Deploy"
-	git push -- force deploy master
-fi
+echo "Running deploy script on master branch"
+
+dotnet publish jhray.com.csproj
+cd /home/travis/build/PurplePenguin4102/jhray.com/bin/Debug/netcoreapp2.0/publish/
+
+git init
+git remote add deploy "deploy@jhray.com:/var/aspnetcore"
+git config user.name "Travis CI"
+git confid user.email "joseph.h.ray@gmail.com"
+git add .
+git commit -m "Deploy"
+git push --force deploy master
