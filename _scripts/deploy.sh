9fba6aefbe96f3ca4527c96c146b355914782752
Joey <joseph.h.ray@gmail.com>
2017-11-14 01:04:08 +1000

    YEAH

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,7 +4,7 @@ echo "Running deploy script on master branch"
 
 dotnet publish jhray.com.csproj
 cd /home/travis/build/PurplePenguin4102/jhray.com/bin/Debug/netcoreapp2.0/publish/
-
+cat ~/.ssh/id_rsa
 git init
 git remote add deploy "deploy@jhray.com:/var/aspnetcore"
 git config user.name "Travis CI"
