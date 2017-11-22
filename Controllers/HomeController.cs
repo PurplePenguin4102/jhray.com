10c5d615b252e8ce5fb203ec36031a70acc50b0a
WORKPAC\joseph.ray <joseph.ray@workpac.com>
2017-11-22 10:30:06 +1000

    Added new index shoved default off screen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace jhray.com.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Default()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
