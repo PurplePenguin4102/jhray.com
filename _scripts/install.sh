d78180c7c39873b65cba7c0e1fec8f26009188e1
WORKPAC\joseph.ray <joseph.ray@workpac.com>
2017-11-23 13:08:23 +1000

    added bundle minifier to project and made scripts a little more informatice

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/.ssh 
 chmod 700 ~/.ssh
 echo "$Deploy_Key" | perl -pe 's/\\n/\n/g' > ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
+echo "Key successfully copied to id_rsa"
 chmod 600 ~/.ssh/id_rsa
  
