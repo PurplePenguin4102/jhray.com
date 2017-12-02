e86ddcabb98314268c2cb0ffb8720074afe105d2
Joey <joseph.h.ray@gmail.com>
2017-12-02 16:15:37 +1000

    thought leader

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,8 @@
 ﻿// Write your JavaScript code.
+var inspirations = ["Thought Leader", "Genius", "Thinker", "Architect", "Teacher", "Possibility Realiser", "Dervish", "Scientist", "Philosopher", "Leader", "Creator", "Dream Enabler", "Physicist", "Generalissimo", "Mind Expander", "Entrepreneur", "Cloud Specialist", "Blockchain", ".Com Developer", "Project Illuminary"];
+setInterval(function () { inspire() }, 1000);
+function inspire() {
+	var text = document.getElementById("thought_leader");
+	var randomInt = Math.floor(Math.random() * inspirations.length);
+	text.innerHTML = inspirations[randomInt];
+}
