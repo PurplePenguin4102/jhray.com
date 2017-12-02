34cec059034f075da705dc0842afc04a973716d7
Joey <joseph.h.ray@gmail.com>
2017-12-02 17:33:42 +1000

    Thinkgineer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,24 @@
 ﻿// Write your JavaScript code.
-var inspirations = ["Thought Leader", "Genius", "Industrialist", "Solver", "Problem Destroyer", "Code Artist", "Thinker", "Architect", "Artisan", "Reality Shaper", "Teacher", "Possibility Realiser", "Dervish", "Scientist", "Philosopher", "Creator", "Dream Enabler", "Physicist", "Generalissimo", "Mind Expander", "Entrepreneur", "Cloud Specialist", "Blockchain", ".Com Developer", "Project Illuminary"];
+var inspirations = ["Thought Leader", "Genius", "Thinkgineer", "Industrialist", "Solver", "Problem Destroyer", "Code Artist", "Thinker", "Architect", "Artisan", "Reality Shaper", "Teacher", "Possibility Realiser", "Dervish", "Scientist", "Philosopher", "Creator", "Dream Enabler", "Physicist", "Generalissimo", "Mind Expander", "Entrepreneur", "Cloud Specialist", "Blockchain", ".Com Developer", "Project Illuminary"];
+var flip = true;
 setInterval(function () { inspire() }, 1000);
+
 function inspire() {
-	var text = document.getElementById("thought_leader");
 	var randomInt = Math.floor(Math.random() * inspirations.length);
-	text.innerHTML = inspirations[randomInt];
+	var text = $("#thought_leader");
+	var flop = flip ? "90" : "0";
+	if (!flip) {
+		text.html(inspirations[randomInt]);
+	}
+		
+	flip = !flip;
+	text.css({
+		transform: "rotatex" + "(" + flop + "deg)",
+		"transition": "all 0.2s ease-out",
+	})
+	if (!flip) {
+		setTimeout(function () {
+			text.html(inspirations[randomInt]);
+		}, 200);
+	}
 }
