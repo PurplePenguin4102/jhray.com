863cfa8d63b05f7603b2d56e65aedd9112db3863
Joey <joseph.h.ray@gmail.com>
2017-12-02 16:33:30 +1000

    Yay

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿// Write your JavaScript code.
-var inspirations = ["Thought Leader", "Genius", "Thinker", "Architect", "Teacher", "Possibility Realiser", "Dervish", "Scientist", "Philosopher", "Leader", "Creator", "Dream Enabler", "Physicist", "Generalissimo", "Mind Expander", "Entrepreneur", "Cloud Specialist", "Blockchain", ".Com Developer", "Project Illuminary"];
+var inspirations = ["Thought Leader", "Genius", "Industrialist", "Solver", "Problem Destroyer", "Code Artist", "Thinker", "Architect", "Artisan", "Reality Shaper", "Teacher", "Possibility Realiser", "Dervish", "Scientist", "Philosopher", "Creator", "Dream Enabler", "Physicist", "Generalissimo", "Mind Expander", "Entrepreneur", "Cloud Specialist", "Blockchain", ".Com Developer", "Project Illuminary"];
 setInterval(function () { inspire() }, 1000);
 function inspire() {
 	var text = document.getElementById("thought_leader");
