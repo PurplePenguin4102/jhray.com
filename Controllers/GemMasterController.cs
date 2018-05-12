091180a79227ab8a88db3efba398306f09971c7e
Joey Ray <joseph.h.ray@gmail.com>
2018-05-12 12:42:37 +1000

    starting login function

---
+++ Controllers/GemMasterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jhray.com.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jhray.com.Controllers
+{
+    public class GemMasterController : Controller
+    {
+        public IActionResult Login()
+        {
+            var usr = new User();
+            return View(usr);
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+
+            return View(user);
+        }
+    }
+}
