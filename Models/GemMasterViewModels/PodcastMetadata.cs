3af180c72ef5e4e860db0c11938e7635c497ce34
Joey <joseph.h.ray@gmail.com>
2018-06-02 21:26:28 +1000

    blim

--- Models/GemMasterViewModels/PodcastMetadata.cs
+++ Models/GemMasterViewModels/PodcastMetadata.cs
@@ -7,5 +7,12 @@ namespace jhray.com.Models.GemMasterViewModels
 {
     public class PodcastMetadata
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        private string Location { get; set; }
+        public string ItunesDuration { get; set; }
+        public DateTimeOffset PubDate { get; set; }
+        private ulong LengthInBytes { get; set; }
     }
 }
