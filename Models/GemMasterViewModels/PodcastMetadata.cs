8dd910d82eb6981092bc79c62e6749224a49d202
Joey <joseph.h.ray@gmail.com>
2018-06-03 20:24:23 +1000

    adding lamo frameworks

--- Models/GemMasterViewModels/PodcastMetadata.cs
+++ Models/GemMasterViewModels/PodcastMetadata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace jhray.com.Models.GemMasterViewModels
         public string ShortDescription { get; set; }
         private string Location { get; set; }
         public string ItunesDuration { get; set; }
+        
+        [DataType(DataType.Date)]
         public DateTimeOffset PubDate { get; set; }
         private ulong LengthInBytes { get; set; }
     }
