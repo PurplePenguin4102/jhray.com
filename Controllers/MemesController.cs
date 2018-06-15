24257a191c76b6633babcebe087196fe60e52251
WORKPAC\joseph.ray <joseph.ray@workpac.com>
2018-06-15 13:08:49 +1000

    blim

---
+++ Controllers/MemesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jhray.com.Controllers
+{
+    public class MemesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToPage("memes/");
+        }
+    }
+}
