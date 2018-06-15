6eae1bb192028e27dc46b032680e190e3d522a4f
WORKPAC\joseph.ray <joseph.ray@workpac.com>
2018-06-15 13:17:52 +1000

    oops

--- Controllers/MemesController.cs
+++ Controllers/MemesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace jhray.com.Controllers
 {
     public class MemesController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return RedirectToPage("memes/");
