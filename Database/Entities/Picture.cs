64e2d8878b1029d195f6a1dc0bc8f85fb518284f
Joey Ray <joseph.h.ray@gmail.com>
2018-07-10 21:53:12 +1000

    hello darkness my old friend

---
+++ Database/Entities/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jhray.com.Database.Entities
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string ArtistLink { get; set; }
+        public string HoverText { get; set; }
+
+        public List<PictureTag> PictureTags { get; set; }
+        public Gem GemData { get; set; }
+    }
+}
---
+++ Database/Entities/PictureTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jhray.com.Database.Entities
+{
+    public class PictureTag
+    {
+        public int Id { get; set; }
+        public string PictureId { get; set; }
+        public string TagText { get; set; }
+
+        public Picture PictureData { get; set; }
+    }
+}
