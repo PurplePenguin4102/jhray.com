a6a7e7d509bace6d24b8740e25b397cf48673182
Joey <joseph.h.ray@gmail.com>
2018-10-15 21:24:46 +1000

    updating db like a boss

---
+++ Migrations/20181015112411_PuttingInOtherFeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jhray.com.Migrations
+{
+    public partial class PuttingInOtherFeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FeedId",
+                table: "Podcasts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "FileSize",
+                table: "Pictures",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FeedId",
+                table: "Podcasts");
+
+            migrationBuilder.DropColumn(
+                name: "FileSize",
+                table: "Pictures");
+        }
+    }
+}
