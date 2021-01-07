ea240c6f723818a49e0b3de912efe1bdd88cb1c5
Joey <joseph.ray@workpac.com>
2021-01-07 07:21:18 -0800

    listening on loopback 5002

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -19,6 +21,10 @@ namespace jhray.com
                 {
                     options.Limits.MaxRequestBodySize = 150_000 * 1024; // 150,000 kB = 150MB
                     options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(30);
+                    options.Listen(IPAddress.Loopback, 5002, listenOptions =>
+                    {
+                        listenOptions.Protocols = HttpProtocols.Http2;
+                    });
                 })
                 .UseStartup<Startup>()
                 .Build().Run();
